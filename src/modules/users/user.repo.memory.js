108c3d3b95d3ebd2b55da6d03ca563f044a3a27f
louiz8022 <ligeirinhook8022@gmail.com>
2025-10-17 18:36:53 -0300

    implementado repositorio do usuario

---
+++ src/modules/users/user.repo.memory.js
@@ -0,0 +1,17 @@
+let users = [] // TODO: Migrar para DB
+
+export const makeUserRepoMemory = () => {
+    const create = async ({ name, email, passwordHash }) => {
+        const id = users.lenght + 1;
+        const user = { id, name, email, passwordHash }
+
+        users.push(user)
+        return user
+
+    }
+    const findbyEmail = ({ email }) => {
+        users.find(user => user.email === email) ?? null
+    }
+
+    return { create, findbyEmail }
+}
