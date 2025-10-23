88d39bf989c9824d9aa5390f9638f6ccca138c7a
louiz8022 <ligeirinhook8022@gmail.com>
2025-10-23 18:44:34 -0300

    implementado esquemas de validaçao de produtos

---
+++ src/modules/products/products.schemas.js
@@ -0,0 +1,25 @@
+import {z} from "zod"
+
+export const createProductSchema = z.object({
+name: z.string().min(1, "name is required"),
+price: z.number().nonnegative("price must be >=0")
+})
+
+export const listProductsQuery = z.object({
+q: z.string().optional(),
+order: z.enum(["id", "name", "price"]).optional(),
+dir:z.enum(["ASC", "DESC"]).optional(),
+page:z.coerce.number().int().positive().optional(),
+limit: z.coerce.number().int().positive().max(100).optional()
+})
+
+export const productIdParams = z.object ({
+    id: z.coerce.number().int().positive()
+})
+
+export const patchProductsSchema = z.object({
+    name: z.string().min(1).optional(),
+    price: z.number().nonnegative().optional() 
+}).refine(obj =>
+    Object.keys(obj).length > 0, { error:'no fields to update'}
+)
