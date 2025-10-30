0c9f810a3f3e76de7863fb6096468a78e33d04f7
louiz8022 <ligeirinhook8022@gmail.com>
2025-10-30 19:42:40 -0300

    modelo do usuario

---
+++ src/models/User.js
@@ -0,0 +1,46 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database";
+
+export const User = sequelize.define("User", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+
+    },
+    name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        validate: {
+            len: {
+                args: [3, 100],
+                msg: "THe name must be betwenn 3 and 100 characters long"
+            }
+
+        }
+    },
+    email: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        unique: {
+            msg: "Email already in use"
+
+        },
+        validate: {
+            isEmail: {
+                msg: "Invalid email"
+            }
+        }
+    },
+    passwordHash: {
+        type: DataTypes.STRING(255),
+        allowNull:false,
+        field: "password_hash"
+
+    }
+}, { tableName:"users",
+    timestamps: true,
+    underscored:true
+
+}
+)
